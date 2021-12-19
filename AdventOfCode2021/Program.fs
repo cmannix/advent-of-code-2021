15f5a202f2608a0b457e74968a6b897597bcb96f
Chris Mannix <chris.mannix@checkout.com>
2021-12-19 21:45:30 +0000

    Format

--- AdventOfCode2021/Program.fs
+++ AdventOfCode2021/Program.fs
@@ -1,23 +1,41 @@
 open System.IO
 
 let day1Result =
- File.ReadLines("Day1/input.txt")
- |> Seq.map int
- |> Seq.fold Day1.countIncreasedDepths { PreviousDepth = None; IncreasedDepthCount = 0 }
+    File.ReadLines("Day1/input.txt")
+    |> Seq.map int
+    |> Seq.fold
+        Day1.countIncreasedDepths
+        { PreviousDepth = None
+          IncreasedDepthCount = 0 }
+
 printfn $"Day 1: Count of increasing depths = %i{day1Result.IncreasedDepthCount}"
 
 let day2Part1Result =
- File.ReadLines("Day2/input.txt")
- |> Seq.map Day2.parseCommand
- |> Seq.fold Day2.updatePositionPart1 { HorizontalPosition = 0; Depth = 0; Aim = 0 }
- |> fun finalPosition -> finalPosition.Depth * finalPosition.HorizontalPosition
+    File.ReadLines("Day2/input.txt")
+    |> Seq.map Day2.parseCommand
+    |> Seq.fold
+        Day2.updatePositionPart1
+        { HorizontalPosition = 0
+          Depth = 0
+          Aim = 0 }
+    |> fun finalPosition ->
+        finalPosition.Depth
+        * finalPosition.HorizontalPosition
+
 printfn $"Day 2 (Part 1): Product of final horizontal and vertical positions = %i{day2Part1Result}"
 
 let day2Part2Result =
- File.ReadLines("Day2/input.txt")
- |> Seq.map Day2.parseCommand
- |> Seq.fold Day2.updatePositionPart2 { HorizontalPosition = 0; Depth = 0; Aim = 0 }
- |> fun finalPosition -> finalPosition.Depth * finalPosition.HorizontalPosition
+    File.ReadLines("Day2/input.txt")
+    |> Seq.map Day2.parseCommand
+    |> Seq.fold
+        Day2.updatePositionPart2
+        { HorizontalPosition = 0
+          Depth = 0
+          Aim = 0 }
+    |> fun finalPosition ->
+        finalPosition.Depth
+        * finalPosition.HorizontalPosition
+
 printfn $"Day 2 (Part 2): Product of final horizontal and vertical positions = %i{day2Part2Result}"
 
 let day3Part1Result =
